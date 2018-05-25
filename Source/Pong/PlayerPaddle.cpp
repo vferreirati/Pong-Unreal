ffec259df1992d25048d599e5b91066a2b92fe46
Victor Ferreira <vferreirati@gmail.com>
2018-05-25 10:05:06 -0300

    EnemyPaddle Actor and PlayerPaddle Pawn created.

---
+++ Source/Pong/PlayerPaddle.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerPaddle.h"
+
+
+// Sets default values
+APlayerPaddle::APlayerPaddle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerPaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPaddle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
