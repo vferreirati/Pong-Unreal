e28777c862e4a26a9a5e3782a9924ce95b60f945
Victor Ferreira <vferreirati@gmail.com>
2018-05-25 10:03:53 -0300

    First commit

---
+++ Source/Pong/PongGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PONG_API APongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
